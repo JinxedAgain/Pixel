1b791198074799786e6707989a26a919e1bcd9e0
Jinxed <96298633+JinxedAgain@users.noreply.github.com>
2022-03-08 11:05:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,45 @@
-# Import 
-import discord
-import os
-import asyncio
-# From
-from ast import alias
-from asyncio.windows_events import NULL
-from cgitb import text
-from email import message
-from math import remainder
-from discord.ext import commands, tasks
-from datetime import datetime, timedelta
-from itertools import count, cycle
-from aiohttp import request
-from pyparsing import null_debug_action
-from bs4 import BeautifulSoup
-from discord.ext import commands
-from discord.utils import find
-
-#Prefix '!'
-client = commands.Bot(command_prefix='!')
-
-client.remove_command("help")
-
-status = cycle(
-    ['Try !help','Prefix - !'])
-
-@client.event
-async def on_ready():
-    change_status.start()
-    print("Bot is Online!")
-
-@tasks.loop(seconds=10)
-async def change_status():
-    await client.change_presence(activity=discord.Game(next(status)))
-
 ###################################################################
-#MAIN CODE
+# AUTO REMIND
 ###################################################################
 
-###################################################################
-# 6HR REMIND
-###################################################################
-
-@client.command()
-async def reminder(ctx):
+@client.command(aliases=['AR'])
+async def ar(ctx):
+    checkmark = '✅'
+    X_No = '❌'
+    hourglass = '⌛'
     message_channel = client.get_channel(941022968534941777) #Channel ID
-    createEmbed = discord.Embed(title='Have you eaten today?', description='✅ Yes\n❌ No\n⌛ Remind me in "**X**"')
-    message = await message_channel.send(embed = createEmbed, delete_after=30)
-    await message.add_reaction('✅')
-    await message.add_reaction('❌')
-    await message.add_reaction('⌛')
+    seconds = 0
+    createEmbed = discord.Embed(title='Have you eaten today?', description=f'{checkmark} Yes\n{X_No} No\n⌛ Remind me in "**X**"')
+    message = await ctx.send(embed = createEmbed, delete_after=30)
+    await message.add_reaction(f'{checkmark}')
+    await message.add_reaction(f'{X_No}')
+    await message.add_reaction(f'{hourglass}')
+
+    def check(reaction, user):
+        return user == ctx.author and str(
+            reaction.emoji) in [checkmark, X_No, hourglass]
 
     i = 0
     reaction = None
 
     while True:
-        if str(reaction) == '✅':
+        if str(reaction) == f'{checkmark}':
             i = 0
-            await message_channel.send('Amazing I will let your friends know!', delete_after=30)
+            await ctx.send('Amazing I will let your friends know!', delete_after=30)
             # Add function where it dms users from a list eg. @user ate some food today congratulate them!...
-        elif str(reaction) == '❌':
+        elif str(reaction) == f'{X_No}':
             if i > 0:
                 i -= 1
-            await message_channel.send('Thats too bad... Try again tomorrow!', delete_after=30)
+            await ctx.send('Thats too bad... Try again tomorrow!', delete_after=30)
             # Add function where it dms users from a list eg. @user didnt eat today give them some motivation... 
-        elif str(reaction) == '⌛':
+        elif str(reaction) == f'{hourglass}':
             if i < 2:
                 i += 1
             await message_channel.send('Ok! How long until I should remind you again:', delete_after=30)
-            time = time
-            seconds = 0
+            def check(m):
+                return m.author == message.author and m.channel == message.channel
+            mesg = await client.wait_for("message", check=check, timeout=60)
+            time = mesg
             if time.lower().endswith("d"):
                 seconds += int(time[:-1]) * 60 * 60 * 24
                 counter = f"{seconds // 60 // 60 // 24} Days"
@@ -84,13 +54,16 @@ async def reminder(ctx):
                 counter = f"{seconds} Seconds"
             if seconds == 0 or seconds > 7776000:
                 await message_channel.send("Please specify a valid time!")
-                msginput = await client.wait_for("message", check=check)
-                if str(msginput.content) == time:
-                    await message_channel.send(f"Alright I will remind you again in {counter}")
+            if str(mesg.content) == time:
+                await message_channel.send(f"Alright I will remind you again in {counter}")
                 await asyncio.sleep(counter)
-                while(True):
-                    if(reminder(input())):
-                        break 
+            else:
+                await message_channel.send(f"There Was an Unexpected Error")
+                    
+        try:
+            reaction, user = await client.wait_for("reaction_add", timeout=10.0, check=check)
+        except:
+            break
 
 #Then loop again in the amount of hours.  
 
@@ -124,9 +97,3 @@ async def reminder(ctx):
 #     elif emoji == '❌':
 #         await user.send('**I will continue reminding you.**', delete_after=30)
 #         #cancel() #define function that will break loop and start next day
-
-###################################################################
-#Discord Bot Token
-token = "TOKEN"
-client.run(token)
-###################################################################
